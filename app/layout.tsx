6f8f4a38d8a5ae2856f0d251dae0bb197b738f38
Gurpreet <gs109690@gmail.com>
2025-11-29 16:02:46 +0530

    alchemy setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,33 @@
+import { config } from "@/config";
+import { cookieToInitialState } from "@account-kit/core";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+import { headers } from "next/headers";
 import "./globals.css";
+import { Providers } from "@/components/providers/Providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Alchemy Wallets",
   description: "Difference between EOA vs Alchemy Wallets",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headersList = await headers();
+  const initialState = cookieToInitialState(
+    config,
+    headersList.get("cookie") ?? undefined
+  );
+
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <Providers initialState={initialState}>{children}</Providers>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,37 @@
-import Image from "next/image";
+"use client";
+import {
+  useAuthModal,
+  useLogout,
+  useSignerStatus,
+  useUser,
+} from "@account-kit/react";
 
 export default function Home() {
+  const user = useUser();
+  const { openAuthModal } = useAuthModal();
+  const signerStatus = useSignerStatus();
+  const { logout } = useLogout();
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Alchemy Wallets
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Difference between EOA vs Alchemy Wallets.
-          </p>
+    <main className="flex min-h-screen flex-col items-center p-24 gap-4 justify-center text-center">
+      {signerStatus.isInitializing ? (
+        <>Loading...</>
+      ) : user ? (
+        <div className="flex flex-col gap-2 p-2">
+          <p className="text-xl font-bold">Success!</p>
+          You're logged in as {user.email ?? "anon"}.
+          <button
+            className="akui-btn akui-btn-primary mt-6"
+            onClick={() => logout()}
+          >
+            Log out
+          </button>
         </div>
-      </main>
-    </div>
+      ) : (
+        <button className="akui-btn akui-btn-primary" onClick={openAuthModal}>
+          Login
+        </button>
+      )}
+    </main>
   );
 }
