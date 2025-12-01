fb7af9521b797d40430c3049dd219d73f093a0a1
Gurpreet <gs109690@gmail.com>
2025-12-01 22:08:33 +0530

    setup viem and wagmi

--- components/providers/Providers.tsx
+++ components/providers/Providers.tsx
@@ -6,6 +6,8 @@ import {
 } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
+import { wagmiConfig } from "@/wagmiConfig";
+import { WagmiProvider } from "wagmi";
 
 export const Providers = (
   props: PropsWithChildren<{
@@ -13,14 +15,16 @@ export const Providers = (
   }>
 ) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AlchemyAccountProvider
-        config={config}
-        queryClient={queryClient}
-        initialState={props.initialState}
-      >
-        {props.children}
-      </AlchemyAccountProvider>
-    </QueryClientProvider>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <AlchemyAccountProvider
+          config={config}
+          queryClient={queryClient}
+          initialState={props.initialState}
+        >
+          {props.children}
+        </AlchemyAccountProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 };
