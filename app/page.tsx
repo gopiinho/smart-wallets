031520e17dbb8f6a61cb201734d55f11fd5ed1d1
Gurpreet <gs109690@gmail.com>
2025-12-01 22:30:23 +0530

    page style update

--- app/page.tsx
+++ app/page.tsx
@@ -14,24 +14,44 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-4 justify-center text-center">
-      {signerStatus.isInitializing ? (
-        <>Loading...</>
-      ) : user ? (
-        <div className="flex flex-col gap-2 p-2">
-          <p className="text-xl font-bold">Success!</p>
-          You're logged in as {user.email ?? "anon"}.
-          <button
-            className="akui-btn akui-btn-primary mt-6"
-            onClick={() => logout()}
-          >
-            Log out
-          </button>
+      <div className="flex flex-col gap-10">
+        <h1 className="font-semibold text-xl">Smart Wallets</h1>
+        <div className="flex gap-3 mx-auto justify-between w-xl">
+          <div className="flex flex-col gap-4 p-4">
+            <h2>Alchemy</h2>
+            {signerStatus.isInitializing ? (
+              <>Loading...</>
+            ) : user ? (
+              <div className="flex flex-col gap-2 p-2">
+                <p className="text-xl font-bold">Success!</p>
+                You're logged in as {user.email ?? "anon"}.
+                <button
+                  className="akui-btn akui-btn-primary mt-6"
+                  onClick={() => logout()}
+                >
+                  Log out
+                </button>
+              </div>
+            ) : (
+              <button
+                className="akui-btn akui-btn-primary"
+                onClick={openAuthModal}
+              >
+                Login
+              </button>
+            )}
+          </div>
+          <div className="flex flex-col gap-4 p-4">
+            <h2>EOA</h2>
+            <button
+              className="akui-btn akui-btn-primary"
+              onClick={openAuthModal}
+            >
+              Connect
+            </button>
+          </div>
         </div>
-      ) : (
-        <button className="akui-btn akui-btn-primary" onClick={openAuthModal}>
-          Login
-        </button>
-      )}
+      </div>
     </main>
   );
 }
