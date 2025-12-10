d1378262bba6f5e825e9e6156005606b9c7f914b
Gurpreet <gs109690@gmail.com>
2025-12-10 13:31:53 +0530

    update config and provider

--- components/providers/Providers.tsx
+++ components/providers/Providers.tsx
@@ -6,8 +6,6 @@ import {
 } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
-import { wagmiConfig } from "@/wagmiConfig";
-import { WagmiProvider } from "wagmi";
 
 export const Providers = (
   props: PropsWithChildren<{
@@ -15,16 +13,14 @@ export const Providers = (
   }>
 ) => {
   return (
-    <WagmiProvider config={wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
-        <AlchemyAccountProvider
-          config={config}
-          queryClient={queryClient}
-          initialState={props.initialState}
-        >
-          {props.children}
-        </AlchemyAccountProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <QueryClientProvider client={queryClient}>
+      <AlchemyAccountProvider
+        config={config}
+        queryClient={queryClient}
+        initialState={props.initialState}
+      >
+        {props.children}
+      </AlchemyAccountProvider>
+    </QueryClientProvider>
   );
 };
--- config.ts
+++ config.ts
@@ -1,24 +1,24 @@
 // @noErrors
 import { createConfig, cookieStorage } from "@account-kit/react";
 import { QueryClient } from "@tanstack/react-query";
-import { arbitrumSepolia, alchemy } from "@account-kit/infra";
+import { baseSepolia, alchemy } from "@account-kit/infra";
 
 export const config = createConfig(
   {
     transport: alchemy({
       apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY!,
     }),
-    chain: arbitrumSepolia,
+    chain: baseSepolia,
     ssr: true,
     storage: cookieStorage,
     enablePopupOauth: true,
     // Learn more here: https://www.alchemy.com/docs/wallets/transactions/sponsor-gas
-    policyId: process.env.NEXT_PUBLIC_ALCHEMY_POLICY_ID,
+    policyId: process.env.NEXT_PUBLIC_ALCHEMY_POLICY_ID!,
   },
   {
     auth: {
       sections: [
-        [{ type: "email" }],
+        [{ type: "email", emailMode: "otp" }],
         [
           { type: "passkey" },
           { type: "social", authProviderId: "google", mode: "popup" },
